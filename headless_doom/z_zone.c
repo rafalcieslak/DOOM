a2fe252f26281d6dd5cc5d2127ecc70ce37ae130
Jack Whitham <jack.d.whitham@gmail.com>
2016-10-02 01:11:40 +0100

    remove rcsid[] variables

--- headless_doom/z_zone.c
+++ headless_doom/z_zone.c
@@ -22,8 +22,6 @@
 //-----------------------------------------------------------------------------
 
 #include <stdint.h>
-static const char
-rcsid[] = "$Id: z_zone.c,v 1.4 1997/02/03 16:47:58 b1 Exp $";
 
 #include "z_zone.h"
 #include "i_system.h"
